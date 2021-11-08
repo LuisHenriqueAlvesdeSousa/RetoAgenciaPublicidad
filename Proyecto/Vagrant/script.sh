46dea47ea76085108b7072aaba422e76ea3cc3d5
Aitor <2gdaw02@a224p02.arriagainfo.local>
2021-11-08 14:02:56 +0100

    script 1

---
+++ Proyecto/Vagrant/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt update
+sudo apt install apache2 -y
+sudo apt install openssh-server -y
+sudo apt install vsftpd -y
+sudo apt install php libapache2-mod-php php-mysql -y
+sudo apt install mysql-server -y
