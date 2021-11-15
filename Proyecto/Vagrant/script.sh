f1f9c519709b68889763a570ef4f62cca8c02a74
GuillermoMediero <guillermo.mediero@ikasle.egibide.org>
2021-11-15 09:52:43 +0100

    script 1

--- Proyecto/Vagrant/script.sh
+++ Proyecto/Vagrant/script.sh
@@ -5,26 +5,28 @@ sudo apt-get update
 #Instalacion de ssh y claves publicas y privadas
 sudo apt install openssh-server -y
 ssh-keygen
-ssh-copy-id 2gdaw01@172.20.224.101
+
 
 #Instalacion y configuracion de apache2
 sudo apt install apache2 -y
-cp index.php /var/www/html/
-cp error.php /var/www/html/
-cp 000-default.conf /etc/apache2/sites-available/
+#cp index.php /var/www/html/
+#cp error.php /var/www/html/
+sudo cp /vagrant_data/000-default.conf /etc/apache2/sites-available/000-default.conf
 
 #Intalacion y configuracion de FTP
 sudo apt install vsftpd -y
-cp vsftpd.conf /etc/
+sudo cp /vagrant_data/vsftpd.conf /etc/vsftpd.conf
 sudo service vsftpd restrart
 
 #Intalacion y configuracion de 
 sudo apt install php libapache2-mod-php php-mysql -y
-cp dir.conf /etc/apache2/mods-enabled/
+sudo cp /vagrant_data/dir.conf /etc/apache2/mods-enabled/dir.conf
 sudo service apache2 restrart
 
 #Instalacion y configuracion de mysql
 sudo apt install mysql-server -y
-sudo mysql_secure_installation
+sudo service mysql restrart
+sudo mysql < /vagrant_data/retoPuliGrupo1.sql
+
 
 
